8432323724bbbdffc1540d5302867ed48d3bce15
Titova-Anastasiya <110681363+Stasya333@users.noreply.github.com>
2023-06-01 18:26:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,25 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+static const int fact[13] = {
+    1, 1, 2, 6, 24, 120, 720, 5040, //
+    40320, 362880, 3628800, 39916800, 479001600,
+};
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    // напишите реализацию
+    if (tree.getSize() == 0 || n > fact[tree.getSize()])
+        return {};
+
+    if (tree.getSymb() == '*')
+        n--;
+
+    std::vector<char> result;
+    int temp = fact[tree.getSize() - 1];
+
+    result.push_back(tree[n / temp].getSymb());
+    std::vector<char> a = getPerm(tree[n / temp], n % temp);
+    result.insert(result.end(), a.begin(), a.end());
+
+    return result;
 }
